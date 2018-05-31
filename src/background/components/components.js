032d9691ed9f4a984a6b02611208e8eb51b5676d
Dylan <dquesada186@gmail.com>
2018-05-31 15:03:12 -0500

    Added background component

---
+++ src/background/components/components.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Background extends Component{
+    render(){
+        return(
+            <div className="Background">
+                <p className="Background-story">
+                    My name is Dylan Quesada and I am a full-stack developer located in Milwaukee, WI. 
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Background;
